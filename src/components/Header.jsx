7b27360748459bd7008c919e701acb9338168042
Mahmudhasan <mahmudhasan.hb@gmail.com>
2024-01-29 23:32:24 +0600

    Added Our name

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,12 @@ import React from 'react'
 
 export default function Header() {
   return (
-    <div>Header</div>
+    <div>
+      Header
+      <h1>Mahmud Hasan Siddique</h1>
+      <h1>Anik Das</h1>
+      <h1>Sakib Mahmud</h1>
+    </div>
+
   )
 }
