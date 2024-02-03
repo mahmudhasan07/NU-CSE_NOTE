bfaf4c9331803de7dfe109914b2a16dc05f3bb14
anikdas <anikdas842@gmail.com>
2024-02-03 10:40:36 +0600

    Header file updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,9 +4,8 @@ export default function Header() {
   return (
     <div>
       Header
-      <h1>Mahmud Hasan Siddique</h1>
-      <h1>Anik Chandra Das change</h1>
-      <h1>Sakib Mahmud</h1>
+      <h1>NU CSE NOTE</h1>
+	  
     </div>
   )
 }
